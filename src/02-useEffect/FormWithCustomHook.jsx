458499f09f80f9c35487b7271cf0c98b749d30cf
Jessica <calajess@gmail.com>
2024-09-13 21:07:08 -0600

    adding all hooks and features worked

---
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react"
+import { useForm } from "../hooks/useForm";
+
+export const FormWithCustomHook = () => {
+
+    const { formState, onInputChange, onResetForm, username, email, password } = useForm({
+        username: '',
+        email: '',
+        password: ''
+    });
+
+
+
+    useEffect( ()=>{
+        //console.log('useEffect called');
+    } ,[]); //el segundo argumento es para que se llame con el arreglo de dependencias es vacio, es decir, una sola vez
+
+    useEffect( ()=>{
+        //console.log('formState changed');
+    } ,[formState])
+
+    useEffect( ()=>{
+        //console.log('email changed');
+    } ,[email])
+
+  return (
+    <>
+    <h1>Formulario con Custom Hook</h1>
+    <hr />
+    <input
+    type="text"
+    className="form-control"
+    placeholder="Username"
+    name="username"
+    value={username}
+    onChange={ onInputChange}
+
+    >
+    </input>
+    <input
+    type="email"
+    className="form-control mt-2"
+    placeholder="fernando@google.com"
+    name="email"
+    value={email}
+    onChange={onInputChange}
+    >
+    </input>
+
+    <input
+    type="password"
+    className="form-control mt-2"
+    placeholder="Contraseña"
+    name="password"
+    value={password}
+    onChange={onInputChange}
+    >
+    </input>
+
+    <button className="btn btn-primary mt-2" onClick={onResetForm}>Borrar</button>
+    </>
+  )
+}
